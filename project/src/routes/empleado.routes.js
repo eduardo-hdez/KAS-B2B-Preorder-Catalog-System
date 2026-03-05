5871353880538f57d6291ef97b77b554f28e0e10
eduardo-hdez <ehdez2004@gmail.com>
2026-03-05 12:47:18 -0600

    [Feat] añadir vista gestion productos

--- project/src/routes/empleado.routes.js
+++ project/src/routes/empleado.routes.js
@@ -9,4 +9,8 @@ router.get('/catalogo', (request, response) => {
   response.render('empleado/catalogo_productos', { title: 'Catalogo de Productos' });
 });
 
+router.get('/gestion_productos', (request, response) => {
+  response.render('empleado/gestion_productos', { title: 'Gestión de Productos' });
+});
+
 module.exports = router;
