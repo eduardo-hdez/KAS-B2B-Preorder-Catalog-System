841d0c30a7a5ae0d4cc48e056c142dc017c94b24
eduardo-hdez <ehdez2004@gmail.com>
2026-03-05 11:04:41 -0600

    [Feat] hacer referencia a otras paginas para navegacion

--- project/app.js
+++ project/app.js
@@ -7,8 +7,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
 
-app.use((request, response, next) => {
-  response.render('index', { title: 'Navbar Empleado' });
+app.get('/', (request, response) => {
+  response.render('catalogo_productos', { title: 'Catalogo de Productos' });
+});
+
+app.get('/catalogo', (request, response) => {
+  response.render('catalogo_productos', { title: 'Catalogo de Productos' });
+});
+
+app.get('/carrito', (request, response) => {
+  response.render('carrito_reserva', { title: 'Carrito' });
+});
+
+app.get('/detalle_producto', (request, response) => {
+  response.render('detalle_producto', { title: 'Detalle de Producto' });
+});
+
+app.get('/historial_reservas', (request, response) => {
+  response.render('historial_reservas', { title: 'Historial de Reservas' });
+});
+
+app.get('/detalle_pedido', (request, response) => {
+  response.render('detalle_pedido', { title: 'Detalle de Pedido' });
 });
 
 app.listen(3000);
