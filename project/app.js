3ad4bb41080524930003675177869da370690352
DAVGAN513 <A01713432@tec.mx>
2026-03-26 21:14:53 -0600

    [Feat] añadir tiempo de inactividad a la cookie

--- project/app.js
+++ project/app.js
@@ -24,7 +24,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: {httpOnly: true},
+  cookie: {httpOnly: true, maxAge: 1000 * 60 * 30} // 30 minutos
 }));
 
 // Hacer que la variable este disponible en todas las vistas
