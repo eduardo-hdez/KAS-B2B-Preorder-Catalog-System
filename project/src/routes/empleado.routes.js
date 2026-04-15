2c476cf3cca567480178f8cf0796691e113741e4
eduardo-hdez <ehdez2004@gmail.com>
2026-04-14 20:01:55 -0600

    [Feat] modificar rutas empleado para multer middleware
    Co-authored-by: Ana Sofia <A01707156@tec.mx>
    Co-authored-by: Ana Valeria <machucamiranda22@gmail.com>
    Co-authored-by: David <A01713432@tec.mx>

--- project/src/routes/empleado.routes.js
+++ project/src/routes/empleado.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as productoController from '../controllers/producto.controller.js';
-import { requireRol, ROL_EMPLEADO } from '../middleware/auth.middleware.js';
+import {requireRol, ROL_EMPLEADO} from '../middleware/auth.middleware.js';
+import {parseProductoMultipart} from '../middleware/uploadProducto.middleware.js';
 import campanaController from '../controllers/campana.controller.js';
 import * as reservaController from '../controllers/reserva.controller.js';
 
@@ -9,17 +10,21 @@ router.use(requireRol([ROL_EMPLEADO]));
 
 router.get('/', productoController.renderCatalogoEmpleado);
 router.get('/catalogo', productoController.renderCatalogoEmpleado);
-router.post('/catalogo-productos/deshabilitar',productoController.deshabilitarProductosCatalogo);
+router.post('/catalogo-productos/deshabilitar', productoController.deshabilitarProductosCatalogo);
 
 router.get('/detalle-producto', (request, response) => {
-  response.render('empleado/detalle-producto', { title: 'Detalle de Producto' });
+  response.render('empleado/detalle-producto', {title: 'Detalle de Producto'});
 });
 
 router.get('/gestion-productos', productoController.renderGestionProductos);
 
 router.get('/gestion-productos/anadir-producto', productoController.renderAnadirProducto);
 
-router.post('/gestion-productos/anadir-producto', productoController.postAnadirProducto);
+router.post(
+    '/gestion-productos/anadir-producto',
+    parseProductoMultipart,
+    productoController.postAnadirProducto,
+);
 
 router.post('/gestion-productos/deshabilitar', productoController.deshabilitarProductos);
 
@@ -28,11 +33,11 @@ router.post('/gestion-productos/rehabilitar', productoController.rehabilitarProd
 router.get('/tabla-reservas', reservaController.renderTablaReservas);
 
 router.get('/detalle-reserva', (request, response) => {
-  response.render('empleado/detalle-reserva', { title: 'Detalle de Reserva' });
+  response.render('empleado/detalle-reserva', {title: 'Detalle de Reserva'});
 });
 
 router.get('/reporte', (request, response) => {
-  response.render('empleado/reporte', { title: 'Reporte' });
+  response.render('empleado/reporte', {title: 'Reporte'});
 });
 
 router.get('/campanas', campanaController.renderCampanas);
